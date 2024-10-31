4b29bb25afda60dd4527e40f5f6d91c1f4e3f278
Venkata Ramulu Yeruva <114219594+VenkataRamuluYeruva@users.noreply.github.com>
2024-10-31 17:04:17 +0530

    Update YoutubeId.js

--- Backend/routes/YoutubeId.js
+++ Backend/routes/YoutubeId.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const YOUTUBE_API_KEY = 'AIzaSyBPdHay7-t0UH1TdvoxmA9uGCMd57xmBi0';
+const YOUTUBE_API_KEY = 'your api_key';
 const query = 'Godavari ';
 const apiEndpoint = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${YOUTUBE_API_KEY}&maxResults=10`;
 
